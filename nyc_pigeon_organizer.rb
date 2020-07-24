e71f295568dfa78d0cb4efdde103c6e6de34fa61
Jhonny Chamoun <jhonny.chamoun@gmail.com>
2020-07-24 17:14:36 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,72 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_names=collect_names(data)
+  pigeon_list = {}
+  pigeon_names.each { |element|
+    pigeon_list[element] = {:color => [], :gender => [], :lives => []}
+  }
+  
+  append_data(pigeon_list, data)
+  
+  pigeon_list
+end
+
+def collect_names(data)
+  result = []
+  data.each{ |el_key, el_value|
+    #binding.pry
+    el_value.each {|name_key, name_value|
+    #binding.pry
+      result = (result+name_value).uniq
+      #binding.pry
+    }
+  }
+  result
+end
+
+def append_data(pigeon_list, data)
+  data.each{ |el_key, el_value|
+    case el_key
+    when :color
+      append_colors(pigeon_list, el_value)
+    when :gender
+      append_genders(pigeon_list, el_value)
+    when :lives
+      append_habitats(pigeon_list, el_value)
+    else
+      puts "note that #{el_value} weren't mapped"
+    end
+  }
+end
+
+def append_colors(pigeon_list, data)
+  color=""
+  data.each {|color_as_key, names|
+    color = color_as_key.to_s
+    names.each{ |name|
+      pigeon_list[name][:color].push(color)
+    }
+  }
+end
+
+def append_genders(pigeon_list, data)
+  gender=""
+  data.each {|gender_as_key, names|
+    gender = gender_as_key.to_s
+    names.each{ |name|
+      pigeon_list[name][:gender].push(gender)
+    }
+  }
+end
+
+def append_habitats(pigeon_list, data)
+  habitat=""
+  data.each {|habitat_as_key, names|
+    habitat = habitat_as_key.to_s
+    names.each{ |name|
+      pigeon_list[name][:lives].push(habitat)
+    }
+  }
 end
